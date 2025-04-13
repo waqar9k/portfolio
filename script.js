75d798430697346d1753b32607e55aa47a9f0726
WAQAR BALOCH <135479159+waqar9k@users.noreply.github.com>
2025-04-13 13:26:02 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example: Smooth scrolling (optional)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
